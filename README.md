1dd7e2512a107db457df38bff486146e836e4c42
rafaelurben <rafael.urben@gmail.com>
2020-05-25 19:49:26 +0200

    Bugfix?

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Setup:
 
 ```bash
+cd /home/robot
 wget -O bttrc_update.sh https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc_update.sh
 sudo chmod +x bttrc_update.sh
 sudo ./bttrc_update.sh
--- bttrc_update.sh
+++ bttrc_update.sh
@@ -6,25 +6,25 @@
 #
 # Update ausführen
 
-
-mkdir -p bttrc
+mkdir -p /home/robot/ev3-bttrc/bttrc
+cd /home/robot/ev3-bttrc
 
 
 echo "Update gestartet!"
 
 
 echo "Update: Dateien von GitHub herunterladen..."
-wget -q -O bttrc_ev3.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc_ev3.py
-wget -q -O bttrc_update.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc_update.py
-wget -q -O bttrc_calibrate.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc_calibrate.py
+wget -O bttrc_ev3.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc_ev3.py
+wget -O bttrc_update.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc_update.py
+wget -O bttrc_calibrate.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc_calibrate.py
 
 
 cd bttrc
 
-wget -q -O __init__.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc/__init__.py
-wget -q -O chat.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc/chat.py
-wget -q -O morse.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc/morse.py
-wget -q -O printer.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc/printer.py
+wget -O __init__.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc/__init__.py
+wget -O chat.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc/chat.py
+wget -O morse.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc/morse.py
+wget -O printer.py https://raw.githubusercontent.com/rafaelurben/ev3-bttrc/master/bttrc/printer.py
 
 
 cd ../
